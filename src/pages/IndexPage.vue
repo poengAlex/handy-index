d683ee24e9a0285b447cc07d0fac5444ed599430
poengAlex <41318737+poengAlex@users.noreply.github.com>
2022-11-11 16:32:45 +0100

    Name change on front

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,18 +2,16 @@
 	<q-page class="row items-center justify-evenly">
 		<div>
 			<div class="text-h1">
-				Handys script Index
+				Interactive video database
 			</div>
 			<div class="text-caption q-mt-sm">
-				A collection of scripts scripted by the Handy team.
+				A database of scripts scripted by the Handy team.
 			</div>
 			<q-banner class="bg-warning text-black q-mt-lg">
 				<template v-slot:avatar>
 					<q-icon name="warning" color="black" />
 				</template>
-				This page is in BETA. While it is in BETA it is <b>free</b> to use. When complete, this service will
-				have
-				a fee.
+				This site is in BETA.
 			</q-banner>
 		</div>
 	</q-page>
